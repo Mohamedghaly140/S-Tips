3b588fc57c7018e806f4944bf8ec1693a1426425
mohamedghaly140 <mohamedghaly140@gmail.com>
2021-08-06 20:41:44 +0200

    added about page

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,18 @@
-const about = () => {
-	return <div>about</div>;
+import { Fragment } from "react";
+import Head from "next/head";
+import WhoWeArePage from "../components/LandingPages/WhoWeArePage";
+
+const Who = () => {
+	return (
+		<Fragment>
+			<Head>
+				<title>S-Tips | Who we are</title>
+				<meta name="description" content="Who we are" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<WhoWeArePage />
+		</Fragment>
+	);
 };
 
-export default about;
+export default Who;
