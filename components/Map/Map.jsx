99bbe6900fc9cfe1a574f371faeafd54c21b5db9
mohamedghaly140 <mohamedghaly140@gmail.com>
2021-08-07 00:12:24 +0200

    prepare contact page

---
+++ components/Map/Map.jsx
@@ -0,0 +1,38 @@
+import {
+	Marker,
+	GoogleMap,
+	withScriptjs,
+	withGoogleMap,
+} from "react-google-maps";
+
+// 31.440192501079878, 31.67835165277682
+
+const MapGoogle = () => {
+	return (
+		<GoogleMap
+			defaultZoom={15}
+			defaultCenter={{ lat: 31.440192501079878, lng: 31.67835165277682 }}
+		>
+			<Marker position={{ lat: 31.440192501079878, lng: 31.67835165277682 }} />
+		</GoogleMap>
+	);
+};
+
+const MapWrapper = withScriptjs(withGoogleMap(MapGoogle));
+
+const API_KEY = "AIzaSyB1rYD_znfOrNZ4nnZySKTHQUMbJAvTXBI";
+
+const Map = () => {
+	return (
+		<div className="w-100 mt-4" style={{ height: "80vh" }}>
+			<MapWrapper
+				googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=`}
+				loadingElement={<div className="spinner-border" role="status" />}
+				containerElement={<div className="h-100" />}
+				mapElement={<div style={{ height: `100%` }} />}
+			/>
+		</div>
+	);
+};
+
+export default Map;
--- pages/contact.js
+++ pages/contact.js
@@ -1,5 +1,20 @@
+import { Fragment } from "react";
+import Header from "../components/Header/Header";
+
 const contact = () => {
-	return <div>contact</div>;
+	return (
+		<Fragment>
+			<Header />
+			<section className="py-5" style={{ background: "#f5f5f5" }}>
+				<div className="container py-5">
+					<div className="row">
+						<div className="col-md-6"></div>
+						<div className="col-md-6"></div>
+					</div>
+				</div>
+			</section>
+		</Fragment>
+	);
 };
 
 export default contact;
