e586104ff08881aca9491fc4618be0574f5ca2c0
mohamedghaly140 <mohamedghaly140@gmail.com>
2021-08-06 15:29:16 +0200

    fix banner

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,10 +1,12 @@
-// import Image from "next/image";
+import Image from "next/image";
 import styles from "./Header.module.css";
+import banner from "../../assets/slide1.jpg";
 
 const Header = props => {
 	return (
 		<header className={styles.header}>
-			<div className="container">
+			<Image src={banner} layout="fill" alt="banner" placeholder="blur" />
+			<div className="container position-relative" style={{ zIndex: 2 }}>
 				<h1 className={styles.title}>S-Tips</h1>
 				<p className={styles.slogan}>The future of homes at a fingertip</p>
 				<button className={styles.btn}>get started now</button>
