a3ae068b25fb322278309a053bb8b1eb25397aff
mohamedghaly140 <mohamedghaly140@gmail.com>
2021-08-06 20:06:46 +0200

    add active link

--- components/NavItem/NavItem.js
+++ components/NavItem/NavItem.js
@@ -1,9 +1,16 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 import styles from "./NavItem.module.css";
 
 const NavItem = ({ children, href, onClick }) => {
+	const router = useRouter();
+	const { pathname } = router;
+
 	return (
-		<li className={styles.navItem} onClick={onClick}>
+		<li
+			onClick={onClick}
+			className={`${styles.navItem} ${pathname === href && styles.active}`}
+		>
 			<Link className={styles.navLink} href={href}>
 				{children}
 			</Link>
