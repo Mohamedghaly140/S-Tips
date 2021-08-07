3d11d8758d418b5ee277b836d63836093a90166a
mohamedghaly140 <mohamedghaly140@gmail.com>
2021-08-07 18:10:46 +0200

    add gif

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -15,15 +15,15 @@ const Header = props => {
 			/>
 			<div className={styles.overlay} />
 			<div
-				className="container position-relative d-flex align-items-center justify-content-between"
+				className="container position-relative d-flex align-items-center justify-content-between flex-wrap"
 				style={{ zIndex: 3 }}
 			>
-				<div>
+				<div className={styles.caption}>
 					<h1 className={styles.title}>S-Tips</h1>
 					<p className={styles.slogan}>The future of homes at a fingertip</p>
 					<button className={styles.btn}>get started now</button>
 				</div>
-				<Image width={240} height={300} src={mobile} alt="mobile" />
+				<Image width={480} height={300} src={mobile} alt="mobile" />
 			</div>
 		</header>
 	);
